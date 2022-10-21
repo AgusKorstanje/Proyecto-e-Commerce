684f9f1de8fb1c74e722ebabc8a08d258fd427ed
AgusKorstanje <agus.korstanje@gmail.com>
2022-10-20 23:05:00 -0300

    v.2

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const cards = document.getElementById("cards");
 const mostcarrito = document.getElementById("mostrarcarrito");
 const limpiar = document.getElementById("limpiar");
 const carrito = document.getElementById("carrito");
-const Productos = [];
 let Carrito = [];
 let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
 
@@ -17,88 +16,10 @@ if(carritoStorage){
     Carrito = carritoStorage;
 }
 
-class Producto{
-    constructor (info){
-        this.id = info.id;
-        this.nombre = info.nombre;
-        this.precio = info.precio;
-        this.stock = info.stock;
-        this.imagen = info.imagen;
-    }
-}
-
-Productos.push(new Producto(
-    {   
-        id: 1,
-        nombre: "remera",
-        precio: 2000,
-        stock: 17,
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpzd6fy7juijZTShkY4SNIwIG_LYVuJxayn4232ax2_XVhqKCGcnJcbM6BIae5dh3gk2E&usqp=CAU"
-    }
-    )
-)
-Productos.push(new Producto(
-    {   
-        id: 2,
-        nombre: "pantalon",
-        precio: 2500,
-        stock: 14,
-        imagen: "https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw04d7fefa/products/AD_GK8831/AD_GK8831-1.JPG"
-    }
-    )
-)
-Productos.push(new Producto(
-    {   
-        id: 3,
-        nombre: "campera",
-        precio: 4500,
-        stock: 8,
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYiUB-i7UtOjht5BXb2xBRlOiaxS-NMGBSYw&usqp=CAU"
-    }
-    )
-)
-Productos.push(new Producto(
-    {
-        id: 4,
-        nombre: "buzo",
-        precio: 4000,
-        stock: 9,
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ4BItZPETmpiogLa9A0h_5D1ET6adHW-9QA&usqp=CAU"
-    }
-    )
-)
-Productos.push(new Producto(
-    {   
-        id: 5,
-        nombre: "zapatillas",
-        precio: 15000,
-        stock: 2,
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF8zhqCVsXW2ymxwrQSUOO2RPamvyiS844Pw&usqp=CAU"
-    }
-    )
-)
-Productos.push(new Producto(
-    {
-        id: 6,
-        nombre: "medias",
-        precio: 500,
-        stock: 10,
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPqZcPAmz062v0gqIjFzT4a4qNWI9sdpIQQw&usqp=CAU"
-    }
-    )
-)
-Productos.push(new Producto(
-    {
-        id: 7,
-        nombre: "guantes",
-        precio: 700,
-        stock: 7,
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDxZaKIzPudtBvfRzISlFm6a7PGBb8jASQVA&usqp=CAU"
-    }
-    )
-)
-
-Productos.forEach(producto => {
+fetch("./productos/productos.json")
+.then(response => response.json())
+.then(data => data.forEach ( producto => {
+    const {id, nombre, precio, stock, imagen} = producto
     let tarjeta = document.createElement("div")
     tarjeta.innerHTML = `
     <div class="card" style="width: 18rem;">
@@ -113,7 +34,9 @@ Productos.forEach(producto => {
 
     const boton = document.getElementById(producto.id);
     boton.addEventListener("click", () => comprarProducto(producto));
-})
+}
+))
+
 
 actualizarcarrito = () => {
     carrito.innerHTML = "";
@@ -126,6 +49,7 @@ Carrito.forEach(prod=> {
     carrito.append(carro);
     })
 }
+
 
 const comprarProducto = (producto) => {
     Swal.fire({
